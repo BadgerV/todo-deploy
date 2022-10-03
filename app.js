0e8eec505df7aacb417b57a4fabebe7856fc95c7
faozybg <segunfaozan@gmail.com>
2022-10-03 04:51:14 -0700

    commit

--- app.js
+++ app.js
@@ -14,20 +14,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
-    express.static(path.join(__dirname, "./client/build"))
+    express.static(path.join(__dirname, "/client"))
 )
+
+app.get('*', (req, res) => {
+    res.sendFile(
+        path.sendFile(__dirname, "/client/build", "index.html")
+    )
+})
 
 
 const routes = require('./routes/todoRoute');
 app.use('/', routes);
 
 
-app.get('*', (req, res) => {
-    res.sendFile(
-        path.join(__dirname, "./client/build/index.html")
-    )
-})
 
-app.listen(port, () => {
+
+app.listen(process.env.PORT || 7000, () => {
     console.log(`Listening on port ${port}`)
 })
